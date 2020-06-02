1309775fd5c9dbd5a11334ab4fb099634759423f
luo <1973653885@qq.com>
2020-06-02 18:32:52 +0800

    ++

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,7 +1,9 @@
 module.exports = {
   "appName":"BigDipper",
   "envID":"food-note-map-57hku",
+  "envID2":"luo-map-5mmfi",
   "mapSubKey":"5PSBZ-GMSKU-B4TVS-B74EJ-66NST-YFBIX",
+  "mapSubKey2":"HSABZ-2OCR3-KTT3G-YYNJD-OTA4Q-HFFZI",
   "default_scale":16,
   "templateId_friends":"UmS6i-0fJvfTjUcr4VgbE8bfw8whhTntV3dCerxOPJA",
   
